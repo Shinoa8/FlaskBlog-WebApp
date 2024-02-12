7888fbcc86a99c4cbe5bf47220c6431a7fb31236
Touseef Ahmad <touseefahmed9669@gmail.com>
2024-02-12 14:13:32 +0500

    Updated

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '59561e718a913120dd33c48f8ac4faeb'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('EMAIL_USER')
+    MAIL_PASSWORD = os.getenv('EMAIL_PASS')
